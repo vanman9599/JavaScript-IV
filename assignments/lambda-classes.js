a45636dc816ac0d8f8a8c0e611b6d1c4eebe9a25
van jordan <vanjordan46@hotmail.com>
2019-04-04 12:36:52 -0700

    finished prototype-refactor assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,18 @@
 // CODE here for your Lambda Classes
+// * First we need a Person class. This will be our `base-class`
+// * Person receives `name` `age` `location` `gender` all as props
+// * Person receives `speak` as a method.
+// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name`
+//  and `location` are the object's own props
+class Person{
+    constructor(attr){
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+        this.gender = attr.gender;
+    }
+
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
